bc6c3bc35f451ca784905dc7b881a82d0f32fb12
Oleksandr Komendant <avkomq@gmail.com>
2019-07-04 21:47:20 -0400

    Add /hello and /test routes

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
     res.send('Hello, World!');
+});
+
+app.post('/hello', (req, res) => {
+    res.send('You just called POST /hello');
+});
+
+app.all('/test', (req, res) => {
+    res.send(`${req.method} /test`);
 });
 
 module.exports = app;
