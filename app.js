cdf14515b48b6e992270cc6bc0d8a5ca5d3d8acd
Oleksandr Komendant <avkomq@gmail.com>
2019-07-04 22:44:31 -0400

    Three midlewares

--- app.js
+++ app.js
@@ -3,34 +3,19 @@ const things = require('./things');
 
 const app = express();
 
-app.use('/things', things);
-
-app.get('/hello', (req, res) => {
-    res.send('Hello, World!');
+app.use('/', (req, res, next) => {
+    console.log('Before 1');
+    next();
+    console.log('After 1');
 });
 
-app.post('/hello', (req, res) => {
-    res.send('You just called POST /hello');
+app.get('/', (req, res, next) => {
+    res.send('2');
+    next();
 });
 
-app.all('/test', (req, res) => {
-    res.send(`${req.method} /test`);
-});
-
-app.get('/:id', (req, res) => {
-    res.send(`Requested GET /${req.params.id}`);
-});
-
-app.get('/things/:id([0-9]{5})', (req, res) => {
-    res.send(`Called 5 digit /things/${req.params.id}`);
-});
-
-app.get('/:name/:id', (req, res) => {
-    res.send(`Requested name: ${req.params.name} and id: ${req.params.id}`);
-});
-
-app.get('*', (req, res) => {
-    res.send(`Sorry, this is an invalid URL`);
+app.use('/', (req, res) => {
+    console.log('3');
 });
 
 module.exports = app;
