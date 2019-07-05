798ea3fb6d8cc9642389b8b1673bbe73e799829e
Oleksandr Komendant <avkomq@gmail.com>
2019-07-04 22:29:09 -0400

    Add different routes

--- app.js
+++ app.js
@@ -17,4 +17,20 @@ app.all('/test', (req, res) => {
     res.send(`${req.method} /test`);
 });
 
+app.get('/:id', (req, res) => {
+    res.send(`Requested GET /${req.params.id}`);
+});
+
+app.get('/things/:id([0-9]{5})', (req, res) => {
+    res.send(`Called 5 digit /things/${req.params.id}`);
+});
+
+app.get('/:name/:id', (req, res) => {
+    res.send(`Requested name: ${req.params.name} and id: ${req.params.id}`);
+});
+
+app.get('*', (req, res) => {
+    res.send(`Sorry, this is an invalid URL`);
+});
+
 module.exports = app;
